5bc1a844eeefead10fa73bdb3c520d0dd3ac2bd3
Amit Jain <ajain@boisestate.edu>
2021-08-28 18:57:17 -0600

    add insertion sort example

---
+++ examples/module1/insertion-sort/InsertionSort.java
@@ -0,0 +1,27 @@
+
+/**
+ * Insertion sort class. A wrapper class that provides the basic insertion sort.
+ * @author Amit Jain
+ */
+
+public class InsertionSort
+{
+
+/**
+ * Sort the section of the array A[p..r] using Insertion sort.
+ * See Section 2.1 of CLR's Algorithms book.
+ */
+public void insertion_sort(int A[], int p, int r) {
+	for (int j =p +1; j <= r; j++) {
+		int key = A[j];
+		int i = j-1;
+		while ((i > p-1) && (A[i] > key)) {	
+			A[i+1] = A[i];
+			i--;
+		}
+		A[i+1] = key;
+	}
+}
+
+
+}
---
+++ examples/module1/insertion-sort/test1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for n in 10000 20000 40000 80000 160000 320000 640000
+do
+	echo
+		java InsertionSortTest $n $i 
+done
