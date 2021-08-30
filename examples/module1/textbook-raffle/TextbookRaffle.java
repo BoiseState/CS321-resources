76d905e364fdf86501977878751295e42234410a
Amit Jain <ajain@boisestate.edu>
2021-08-30 11:13:40 -0600

    cleanup raffle example

--- examples/module1/textbook-raffle/TextbookRaffle.java
+++ examples/module1/textbook-raffle/TextbookRaffle.java
@@ -16,31 +16,33 @@ public class TextbookRaffle
      * @param raffle An list of raffle participants
      * @throws InterruptedException
      */
-    private static void raffleIt(ArrayList<String> raffle) throws InterruptedException {
-	System.out.println(" Starting the raffle! <low rumbling drum roll> ");
+    private static void raffleIt(ArrayList<String> raffle) throws InterruptedException
+	{
+		System.out.println(" Starting the raffle! <low rumbling drum roll> ");
 
-	for (int i = 0; i < 10; i++) {
-	    System.out.print(" $ ");
-	    Thread.sleep(500); //1/2 sec
-	}
-	System.out.print(" The lucky winner is <drum roll crescendo>" );
-	Thread.sleep(5000); // the suspense
-	System.out.println(":  " + raffle.remove(generator.nextInt(raffle.size())));
-	System.out.println();
+		for (int i = 0; i < 10; i++) {
+	    	System.out.print(" $ ");
+	    	Thread.sleep(500); //1/2 sec
+		}
+		System.out.print(" The lucky winner is <drum roll crescendo>" );
+		Thread.sleep(5000); // the suspense
+		System.out.println(":  " + raffle.remove(generator.nextInt(raffle.size())));
+		System.out.println();
     }
     
-    public static void main(String[] args) throws InterruptedException {		
-	ArrayList<String> raffle = new ArrayList<String>();
-	raffle.add("Austin");
-	raffle.add("Eric");
-	raffle.add("Emily");
-	raffle.add("Sawyer");
-	raffle.add("Jacob");
-	raffle.add("Brandon");
-	raffle.add("Akayou");
-	raffle.add("Raymundo");
+    public static void main(String[] args) throws InterruptedException 
+	{		
+		ArrayList<String> raffle = new ArrayList<String>();
+		raffle.add("Austin");
+		raffle.add("Eric");
+		raffle.add("Emily");
+		raffle.add("Sawyer");
+		raffle.add("Jacob");
+		raffle.add("Brandon");
+		raffle.add("Akayou");
+		raffle.add("Raymundo");
 
-	raffleIt(raffle);
-	raffleIt(raffle);
-    }
+		raffleIt(raffle);
+		raffleIt(raffle);
+	}
 }
