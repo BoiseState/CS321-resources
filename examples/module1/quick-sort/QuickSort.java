fe0cb5f0e61f0fff9d6ae7f0f9f6f29cfff250b3
Amit Jain <ajain@boisestate.edu>
2021-09-26 22:36:17 -0600

    start quicksort example

---
+++ examples/module1/quick-sort/QuickSort.java
@@ -0,0 +1,15 @@
+
+/**
+ * QuikSort class. Provides quicksort.
+ * @author amit
+ */
+
+public class QuickSort
+{
+
+	public void quicksort(int A[], int p, int r) {
+
+	}
+
+
+}
---
+++ examples/module1/quick-sort/README.md
@@ -0,0 +1,3 @@
+
+Experiments with quicksort
+==========================
