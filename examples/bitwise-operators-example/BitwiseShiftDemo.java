73543fe85bbe4546a15ed026a5e99470f78004ca
Amit Jain <ajain@boisestate.edu>
2021-11-10 12:54:11 -0700

    Update bis example

--- examples/bitwise-operators-example/BitwiseShiftDemo.java
+++ examples/bitwise-operators-example/BitwiseShiftDemo.java
@@ -3,7 +3,7 @@
  * Shows an extended example using bitwise operators and DNA sequences.
  * The extended example shows how to pack the bases in a DNA sequence 
  * into a long and how to extract the bases back out. This will result in around 8x savings in memory
- * (and on disk as well)
+ * (and on disk as well). 
  * @author amit
  *
  */
@@ -11,31 +11,30 @@ public class BitwiseShiftDemo
 {
     public static void main(String[] args) {
 
-	long sequence = 0;
+	
 	
 	String dnaSequence = "gatcctccat";
-	dnaSequence = dnaSequence.toUpperCase();
 	int seqLength = dnaSequence.length();
 	System.out.println("original sequence: " + dnaSequence);
 	
+	long sequence = 0;
 	for (int i = 0; i < seqLength; i++) {
 	    sequence = sequence << 2;
 	    switch (dnaSequence.charAt(i)) {
-		case 'A':
+		case 'a':
 		    sequence = sequence | DNA.A;
 		    break;
-		case 'C':
+		case 'c':
 		    sequence = sequence | DNA.C;
 		    break;
-		case 'T':
+		case 't':
 		    sequence = sequence | DNA.T;
 		    break;
-		case 'G':
+		case 'g':
 		    sequence = sequence | DNA.G;
 		    break;
 	    }   
 	}
-	
 	System.out.println(Long.toString(sequence, 2));
 	
 	long value = sequence;
@@ -45,12 +44,12 @@ public class BitwiseShiftDemo
 	
 	while (value > 0) {
 	    //extract the least significant two bits -- which is the DNA base
-	    next = (int) value & 0b11; 
+	    next = (int) value & 0b11; //mask out the 2 least significant bits
 	    switch (next) {
-		case 0: seqString = "A" + seqString; break;
-		case 3: seqString = "T" + seqString; break;    
-		case 1: seqString = "C" + seqString; break;   
-		case 2: seqString = "G" + seqString; break;
+		case 0: seqString = "a" + seqString; break;
+		case 3: seqString = "t" + seqString; break;    
+		case 1: seqString = "c" + seqString; break;   
+		case 2: seqString = "g" + seqString; break;
 	    }
 	    value = value >>> 2;
 	    length++;
