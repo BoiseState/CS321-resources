5aebe9dbf5533ebd5c3b7bdfbed04176c65e12c4
Amit Jain <ajain@boisestate.edu>
2021-11-10 10:41:52 -0700

    updateadd examples for bitwise operators

--- examples/bitwise-operators-example/DNA.java
+++ examples/bitwise-operators-example/DNA.java
@@ -1,8 +1,16 @@
 
+/**
+ * A holding class for constants to represent the DNA bases: A, T, C and G.
+ * Note that values are assigned such that A and T are binary complements of each other 
+ * and so are C and G. This simplifies some of the processing of DNA sequences.
+ * 
+ * @author amit
+ *
+ */
 public class DNA
 {
-    public static final int A = 0b00; // 0
-    public static final int T = 0b11; // 3
-    public static final int C = 0b01; // 1
-    public static final int G = 0b10; // 2
+    public static final int A = 0b00; // 0  Adenine
+    public static final int T = 0b11; // 3  Thiamine
+    public static final int C = 0b01; // 1  Cytosine
+    public static final int G = 0b10; // 2  Guanine
 }
