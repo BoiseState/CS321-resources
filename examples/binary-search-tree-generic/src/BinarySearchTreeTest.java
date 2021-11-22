6f5e823f797712d8b6326e5771abeece2be5048d
Amit Jain <ajain@boisestate.edu>
2021-11-22 11:01:53 -0700

    Update formatting.

--- examples/binary-search-tree-generic/src/BinarySearchTreeTest.java
+++ examples/binary-search-tree-generic/src/BinarySearchTreeTest.java
@@ -1,31 +1,30 @@
 import java.util.Random;
 
 public class BinarySearchTreeTest {
-	
-	private static Random generator = new Random();
-	
-	private static void createRandomTree(int n, BinarySearchTree<Integer> tree) {
-		for (int i = 0; i < n; i++) {
-			int next = generator.nextInt(n);
-			tree.insert(next);
-		}
-	}
 
-	public static void main(String[] args) {
-		
-		if (args.length  == 0) {
-			System.out.println("Usage: java BinarySearchTree <n>");
-			System.exit(1);
-		}
-		int n = Integer.parseInt(args[0]);
+    private static Random generator = new Random();
 
-		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
-		
-		createRandomTree(n, tree);
-		
-		//tree.inorderTreeWalk(tree.getRoot());
-		
-		System.out.println("height of tree = " + tree.getHeight(tree.getRoot()));
+    private static void createRandomTree(int n, BinarySearchTree<Integer> tree) {
+	for (int i = 0; i < n; i++) {
+	    int next = generator.nextInt(n);
+	    tree.insert(next);
 	}
+    }
+
+    public static void main(String[] args) {
+	if (args.length == 0) {
+	    System.out.println("Usage: java BinarySearchTree <n>");
+	    System.exit(1);
+	}
+	int n = Integer.parseInt(args[0]);
+
+	BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+
+	createRandomTree(n, tree);
+
+	// tree.inorderTreeWalk(tree.getRoot());
+
+	System.out.println("height of tree = " + tree.getHeight(tree.getRoot()));
+    }
 
 }
