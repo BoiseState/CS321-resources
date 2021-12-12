c2cabb53a62738efc48dfe266fa312c2bda6a919
amit <ajain@boisestate.edu>
2021-12-11 17:59:10 -0700

    update test scripts and results

--- projects/p4/check-queries.sh
+++ projects/p4/check-queries.sh
@@ -11,13 +11,13 @@ do
 	echo
 	echo "Running queryfile " query$i "on $datafile.btree.data.$i.0"
 	echo
-	time java GeneBankSearchBTree --degree=0 --btreefile=$datafile.btree.data.$i.0 --length=$i --queryfile=queries/query$i --debug=1  > queries/query$i.out
+	time java GeneBankSearchBTree --degree=0 --btreefile=$datafile.btree.data.$i.0 --length=$i --queryfile=queries/query$i --debug=1  > queries/query$i-$datafile.out
 done
 echo
 
 for i in 1 2 3 4 10 20 31
 do
-	diff -w queries/query$i.out results/queries/query$i.out
+	diff -w queries/query$i-$datafile.out results/queries/query$i-$datafile.out
 	if test "$?" = "0"
 	then
 		echo "----> Query-Test-$i PASSED!"
