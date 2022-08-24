90d1405b63212e2269cde98d349d90e2324cee72
amit <ajain@boisestate.edu>
2022-08-24 16:03:51 -0600

    update examples

--- examples/power/BigPowerTest.java
+++ examples/power/BigPowerTest.java
@@ -15,21 +15,21 @@ public class BigPowerTest
 		BigInteger x = new BigInteger(args[0]);
 		int n = Integer.parseInt(args[1]);
 
-		float startTime = NativeTiming.report_cpu_time();
+		long startTime = System.currentTimeMillis();
 		BigInteger result1 = BigPower.power_brute_force(x, n);
-		float totalTime = NativeTiming.report_cpu_time() - startTime;
+		float totalTime = System.currentTimeMillis() - startTime;
 		if (DEBUG) System.out.println(result1);
 		System.err.println("time taken for version 1= "+totalTime+" seconds");
 
-		startTime = NativeTiming.report_cpu_time();
+		startTime = System.currentTimeMillis();
 		BigInteger result2 = BigPower.power_recursive_doubling(x, n);
-		totalTime = NativeTiming.report_cpu_time() - startTime;
+		totalTime = System.currentTimeMillis() - startTime;
 		if (DEBUG) System.out.println(result2);
 		System.err.println("time taken for version 2= "+totalTime+" seconds");
 
-		startTime = NativeTiming.report_cpu_time();
+		startTime = System.currentTimeMillis();
 		BigInteger result3 = x.pow(n);
-		totalTime = NativeTiming.report_cpu_time() - startTime;
+		totalTime = System.currentTimeMillis() - startTime;
 		if (DEBUG) System.out.println(result3);
 		System.err.println("time taken for pow= "+totalTime+" seconds");
 	}
