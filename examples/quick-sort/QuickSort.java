23709ec5a41753f437801a8318c193cc9e34bc61
amit <ajain@boisestate.edu>
2022-09-21 13:25:47 -0600

    reformat code

--- examples/quick-sort/QuickSort.java
+++ examples/quick-sort/QuickSort.java
@@ -10,25 +10,29 @@ public class QuickSort
 
     public void quicksort(int A[], int p, int r) {
 
-	if (p < r) {
-	    int q = partition(A, p, r);
-	    quicksort(A, p, q - 1);
-	    quicksort(A, q + 1, r);
-	}
+        if (p < r) {
+            int q = partition(A, p, r);
+            quicksort(A, p, q - 1);
+            quicksort(A, q + 1, r);
+        }
     }
 
 
     private int partition(int[] A, int p, int r) {
-	int x = A[r];
-	int i = p - 1;
-	for (int j = p; j < r; j++) {
-	    if (A[j] <= x) {
-		i++;
-		int tmp = A[i]; A[i] = A[j]; A[j] = tmp;
-	    }
-	}
-	int tmp = A[i + 1]; A[i + 1] = A[r]; A[r] = tmp;
-	return i + 1;
+        int x = A[r];
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (A[j] <= x) {
+                i++;
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+        }
+        int tmp = A[i + 1];
+        A[i + 1] = A[r];
+        A[r] = tmp;
+        return i + 1;
     }
 
 }
