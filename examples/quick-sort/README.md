ac009f8eaf6d6cd22238f4f86a860066ebf06b56
amit <ajain@boisestate.edu>
2022-09-24 14:05:08 -0600

    update quicksort example

--- examples/quick-sort/README.md
+++ examples/quick-sort/README.md
@@ -1,3 +1,6 @@
 
 Experiments with quicksort
 ==========================
+
+Run the script compare-quicksort-mergesort.sh to compare run times for several instancea for
+quicksort and mergesort.
--- examples/quick-sort/compare-quicksort-mergesort.sh
+++ examples/quick-sort/compare-quicksort-mergesort.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+echo
+echo "Comapring run times for quicksort and mergesort"
+echo
+
 seed=123
 for n in 1000000 5000000 10000000 20000000 30000000 50000000
 do
+	echo
 	java QuickSortTest $n  $seed
 	(cd ../merge-sort/; java MergeSortTest $n 40 sentinels 123)
 done
