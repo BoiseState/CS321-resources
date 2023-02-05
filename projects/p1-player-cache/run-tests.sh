b53f5bb42e118c6cbf149c3859d52b0ccddc8da9
amit <ajain@boisestate.edu>
2023-02-05 12:05:07 -0700

    added starter code and data for p1-player-cache project

--- projects/p1-player-cache/run-tests.sh
+++ projects/p1-player-cache/run-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-javac *.java
 
 if  test -e Player-List50.data  -a -e Player-List1000.data -a -e Player-List100000.data
 then
@@ -8,10 +7,6 @@ then
 	echo "Input data files found. Proceeding with the test!"
 	echo
 else
-	echo
-	echo "Generating input data files: Player-List50.data, Player-List1000.data, and Player-List100000.data!"
-	echo
-	javac PlayerGenerator.java
 	java PlayerGenerator 50 3.0 0 123
 	java PlayerGenerator 1000 15.0 0 123
 	java PlayerGenerator 100000 2500.0 0 123
@@ -24,7 +19,7 @@ do
 	echo "======================================================================="
 	echo "Running Test $i:" $cmd " "
 	$cmd > out$i
-	diff -w -I -B '^Time.*' out$i test-cases/out$i
+	diff -B -w -I '^Time.*' -I "-*" -I "~*" out$i test-cases/out$i
 	result=$?
 	if test "$result" = 0
 	then
