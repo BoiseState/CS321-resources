8a80b47b61111344a15aa00f6442e7d4624d7e77
amit <ajain@boisestate.edu>
2023-10-03 16:17:32 -0600

    added starter code and data for p3-hashing-experiments project

--- projects/p3-hashing-experiments/run-tests.sh
+++ projects/p3-hashing-experiments/run-tests.sh
@@ -29,35 +29,33 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo
 
 
-debugLevel=1
 dos2unix test-cases/* >& /dev/null
+debugLevel=1
+load = 0.5
 
-for load in 0.5 0.6 0.7 0.8 0.9 0.95 0.99
-do
-	echo "Running java HashtableTest dataSource = 3 loadFactor = $load "
-	java HashtableTest 3 $load $debugLevel  >> /dev/null
-	dos2unix linear-dump.txt double-dump.txt  >& /dev/null
+echo "Running java HashtableTest dataSource = 3 loadFactor = $load "
+java HashtableTest 3 $load $debugLevel  >> /dev/null
+dos2unix linear-dump.txt double-dump.txt  >& /dev/null
 
-	echo
-	diff linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
-	if test "$?" = 0
-	then
-		echo "Test PASSED for linear probing and load = $load"
-		/bin/rm -f diff-linear-$load.out
-	else
-		echo "==> Test FAILED for linear probing load = $load!! "
-		echo "       Check the file diff-linear-$load.out for differences"
-	fi
+echo
+diff linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
+if test "$?" = 0
+then
+	echo "Test PASSED for linear probing and load = $load"
+	/bin/rm -f diff-linear-$load.out
+else
+	echo "==> Test FAILED for linear probing load = $load!! "
+	echo "       Check the file diff-linear-$load.out for differences"
+fi
 
-	diff double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
-	if test "$?" = 0
-	then
-		echo "Test PASSED for double probing and load = $load"
-		/bin/rm -f diff-double-$load.out
-	else
-		echo "==> Test FAILED for double probing load = $load!! "
-		echo "       Check the file diff-double-$load.out for differences"
-	fi
-	echo
-done
+diff double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
+if test "$?" = 0
+then
+	echo "Test PASSED for double probing and load = $load"
+	/bin/rm -f diff-double-$load.out
+else
+	echo "==> Test FAILED for double probing load = $load!! "
+	echo "       Check the file diff-double-$load.out for differences"
+fi
+echo
 
