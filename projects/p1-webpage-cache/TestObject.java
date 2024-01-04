c9b49a71d4ca3abaa15c702f295e6e5c2276871e
andrekeys <andrekeys@boisestate.edu>
2024-01-04 10:13:24 -0700

    adding unit tests for project 1

---
+++ projects/p1-webpage-cache/TestObject.java
@@ -0,0 +1,25 @@
+/**
+ * A dummy object that assists with unit tests
+ */
+public class TestObject implements KeyInterface<Integer>{
+
+    private int key;
+
+    public TestObject(int key) {
+        this.key = key;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof TestObject) {
+            TestObject t = (TestObject) o;
+            return t.getKey() == key;
+        } else {
+            return false;
+        }
+    }
+}
