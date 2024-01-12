b05af471c01837115f770c7b7a5b5260bd421d3c
Amit Jain <ajain@boisestate.edu>
2024-01-12 16:49:52 -0700

    added starter code and data for p1-webpage-cache project

--- projects/p1-webpage-cache/CacheTest.java
+++ projects/p1-webpage-cache/CacheTest.java
@@ -103,6 +103,34 @@ class CacheTest {
     }
 
     /**
+     * Tests that Cache uses the MRU scheme by ensuring that an object that was recently referenced
+     * remains in the Cache after adding another.
+     */
+    @Test
+    void mru() {
+
+        cache = new Cache<>(3);
+
+        TestObject t1 = new TestObject(1);
+        TestObject t2 = new TestObject(2);
+        TestObject t3 = new TestObject(3);
+        TestObject t4 = new TestObject(4);
+
+        cache.add(t1);
+        cache.add(t2);
+        cache.add(t3);
+        //At this point, list is t3, t2, t1
+        assertEquals(t1, cache.get(1));
+        //list should be t1, t3, t2
+
+        assertEquals(t2, cache.add(t4));
+        //t2 should have been removed and list should be t4, t1, t3
+
+        assertNull(cache.get(2));
+        assertEquals(t1, cache.get(1));
+    }
+
+    /**
      * When the cache is cleared, there should be nothing in it!
      */
     @Test
