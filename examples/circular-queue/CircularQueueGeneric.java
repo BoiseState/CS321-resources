e0fc38e20177a2239a5021ab4aa25d06978d9622
amit <ajain@boisestate.edu>
2026-02-18 21:28:57 -0700

    update notes

---
+++ examples/circular-queue/CircularQueueGeneric.java
@@ -0,0 +1,119 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * A circular queue implemented in a fixed-size array. This follows the
+ * pseudocode from the book: <a href=
+ * "https://mitpress.mit.edu/9780262046305/introduction-to-algorithms/">Intro to
+ * Algorithms</a> See Section 10.1 and Exercise 10.1-5.
+ * 
+ * @author amit
+ *
+ */
+public class CircularQueueGeneric<E>
+{
+	private E[] Q;
+	private int size = 0;
+	private int head;
+	private int tail;
+
+	/**
+	 * Create a queue that can store a fixed maximum number of elements. We use
+	 * Q[1:n] and ignore Q[0] to follow the pseudocode from the book. The size
+	 * denotes the maximum capacity of the queue, so the queue array size is
+	 * size+1, as we have to leave one space empty for the circular queue to
+	 * work properly.
+	 * 
+	 * @param size
+	 *                 The maximum capacity of the queue.
+	 */
+	@SuppressWarnings("unchecked")
+	public CircularQueueGeneric(Class clazz, int size)
+	{
+		if (size < 1)
+			throw new IllegalArgumentException(
+					"CircularQueue: size must be > 1");
+		this.size = size + 1; // we will use location 1..size and ignore location 0
+
+		Q = (E[]) Array.newInstance(clazz, this.size + 1); 
+		head = tail = 1;
+	}
+
+	/**
+	 * Check if the queue is empty.
+	 * 
+	 * @return true if the queue is empty
+	 */
+	public boolean emptyQueue()
+	{
+		return head == tail;
+	}
+
+	/**
+	 * Add an element to the tail of the queue. If the queue is full, we will
+	 * get an overflow exception as the underlying array is of fixed size.
+	 * 
+	 * @param x
+	 *              the element to add
+	 * @throws QueueOverflow
+	 *                           thrown when queue is full
+	 */
+	public void enqueue(E x) throws QueueOverflow
+	{
+		if (head == (tail + 1) % size)
+			throw new QueueOverflow("CircularQueue: queue is full!");
+		else {
+			Q[tail] = x;
+			if (tail == size)
+				tail = 1; // wrap around
+			else
+				tail = tail + 1;
+		}
+	}
+
+	/**
+	 * Remove the head element from the queue. If the queue is empty, we will
+	 * get an underflow exception.
+	 * 
+	 * @return the element at the head of the queue
+	 * @throws QueueUnderflow
+	 *                            thrown when queue is empty
+	 */
+	public E dequeue() throws QueueUnderflow
+	{
+		E x = null;
+		if (emptyQueue())
+			throw new QueueUnderflow("CircularQueue: queue is empty!");
+		else {
+			x = Q[head];
+			if (head == size)
+				head = 1;
+			else
+				head = head + 1;
+		}
+		return x;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String toString()
+	{
+		String value = "Queue[";
+		int index = head;
+
+		while (index != tail) {
+			value += Q[index] + ", ";
+			index = index + 1;
+			// break if we are at the last element in the queue
+			if ((index == tail) || (index == size && tail == 1)) {
+				break;
+			}
+			if (index == size)
+				index = 1;
+		}
+		value += Q[index] + "]";
+		return value;
+	}
+
+}
--- examples/circular-queue/CircularQueueTest.java
+++ examples/circular-queue/CircularQueueTest.java
@@ -8,74 +8,84 @@
  */
 public class CircularQueueTest
 {
-    /**
-     *  Run a simple smoke test. Add 10 elements, check queue, then remove ten elements
-     *  and check queue again.
-     */
-    private static void smokeTest() {
-        CircularQueue queue = new CircularQueue(10);
-        try {
-            System.out.println("CircularQueueTest: Enqueueing 10 elements...");
-            for (int i = 1; i <= 10; i++)
-                queue.enqueue(i);
-            System.out.println("CircularQueueTest: Current state of queue.");
-            System.out.println(queue);
-         
-            System.out.println("CircularQueueTest: Dequeuing 10 elements.");
-            for (int i = 1; i <= 10; i++)
-                System.out.print(" " + queue.dequeue());
-            System.out.println();
-            System.out.println("CircularQueueTest: Current state of queue.");
-            System.out.println(queue);
-            
-        } catch (QueueOverflow e) {
-            e.printStackTrace();
-        } catch (QueueUnderflow e) {
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * Check for underflow by doing a dequeue on an empty queue.
-     */
-    private static void testUnderflow() {
-        CircularQueue queue = new CircularQueue(1);
-        try {
-            queue.dequeue();
-        } catch (QueueUnderflow e) {
-            e.printStackTrace();
-            System.out.println("CircularQueueTest: caught underflow on an empty queue");
-        }
-    }
-    
-    /**
-     *  Check for overflow by doing an enqueue into a full queue.
-     */
-    private static void testOverflow() {
-        CircularQueue queue = new CircularQueue(1);
-            try {
-                queue.enqueue(1);
-                queue.enqueue(2); //this should trigger an overflow
-            } catch (QueueOverflow e) {
-                e.printStackTrace();
-                System.out.println("CircularQueueTest: caught overflow on a full queue");
-            }    
-    }
-    /**
-     * Run various tests.
-     * @param args no command line arguments used at present
-     */
-    public static void main(String[] args) {
-        System.out.println("CircularQueueTest: a simple smoke test with 10 elements");
-        smokeTest();
-        System.out.println();
-        
-        System.out.println("CircularQueueTest: testing underflow...");
-        testUnderflow();
-        System.out.println();
-        
-        System.out.println("CircularQueueTest: testing overflow...");
-        testOverflow();
-        System.out.println();
-    }
+	/**
+	 * Run a simple smoke test. Add 10 elements, check queue, then remove ten
+	 * elements and check queue again.
+	 */
+	private static void smokeTest()
+	{
+		CircularQueue queue = new CircularQueue(10);
+		try {
+			System.out.println("CircularQueueTest: Enqueueing 10 elements...");
+			for (int i = 1; i <= 10; i++)
+				queue.enqueue(i);
+			System.out.println("CircularQueueTest: Current state of queue.");
+			System.out.println(queue);
+
+			System.out.println("CircularQueueTest: Dequeuing 10 elements.");
+			for (int i = 1; i <= 10; i++)
+				System.out.print(" " + queue.dequeue());
+			System.out.println();
+			System.out.println("CircularQueueTest: Current state of queue.");
+			System.out.println(queue);
+
+		} catch (QueueOverflow e) {
+			e.printStackTrace();
+		} catch (QueueUnderflow e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Check for underflow by doing a dequeue on an empty queue.
+	 */
+	private static void testUnderflow()
+	{
+		CircularQueue queue = new CircularQueue(1);
+		try {
+			queue.dequeue();
+		} catch (QueueUnderflow e) {
+			e.printStackTrace();
+			System.out.println(
+					"CircularQueueTest: caught underflow on an empty queue");
+		}
+	}
+
+	/**
+	 * Check for overflow by doing an enqueue into a full queue.
+	 */
+	private static void testOverflow()
+	{
+		CircularQueue queue = new CircularQueue(1);
+		try {
+			queue.enqueue(1);
+			queue.enqueue(2); // this should trigger an overflow
+		} catch (QueueOverflow e) {
+			e.printStackTrace();
+			System.out.println(
+					"CircularQueueTest: caught overflow on a full queue");
+		}
+	}
+
+	/**
+	 * Run various tests.
+	 * 
+	 * @param args
+	 *                 no command line arguments used at present
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println(
+				"CircularQueueTest: a simple smoke test with 10 elements");
+		smokeTest();
+		System.out.println();
+
+		System.out.println("CircularQueueTest: testing underflow...");
+		testUnderflow();
+		System.out.println();
+
+		System.out.println("CircularQueueTest: testing overflow...");
+		testOverflow();
+		System.out.println();
+	}
 }
